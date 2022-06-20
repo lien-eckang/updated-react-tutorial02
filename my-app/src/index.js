8c9d8fa75854c639bf9a868961a77cce9faf3354
lien-eckang <euncho.kang@lien.kr>
2022-06-20 14:34:29 +0900

    Board의 handleClick 함수 수정
    - Board의 handleClick 함수 수정하여 xIsNext값 뒤집기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -28,6 +28,16 @@ class Square extends React.Component {
         xIsNext: true,
       };
     }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? "X" : "O";
+      this.setState({
+        squares: squares, 
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+
     renderSquare(i) {
       return <Square value={i} />;
     }
