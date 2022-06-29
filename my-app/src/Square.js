732f5d8ac93911dbc7662cd8a46e08fd57fb70a8
lien-eckang <euncho.kang@lien.kr>
2022-06-29 13:19:31 +0900

    Square.js 리팩토링 및 수정
    - Square.js 리팩토링 및 수정

--- my-app/src/Square.js
+++ my-app/src/Square.js
@@ -1,14 +1,11 @@
-import React from 'react';
+import React from "react";
 
-export class Square extends React.Component {
-    render() {
-      return (
-        <button 
-        className="square" 
-        onClick={() => this.props.onClick()}
-        >
+export default class Square extends React.Component {
+  render() {
+    return (
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.props.value}
-        </button>
-      );
-    }
+      </button>
+    );
+  }
 }
